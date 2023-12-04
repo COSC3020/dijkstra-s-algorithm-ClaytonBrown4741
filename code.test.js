9cfe9c6bfd2f40218ea4eb7ea85783bf736138b8
Clayton Brown <111392580+ClaytonBrown4741@users.noreply.github.com>
2023-12-04 14:12:16 -0700

    Rewrote tests to use for new program

--- code.test.js
+++ code.test.js
@@ -1,25 +1,24 @@
 const fs = require('fs');
 const assert = require('assert');
+
 eval(fs.readFileSync('code.js')+'');
-dm = {};
-assert(JSON.stringify(initializeDAlgorithm(dm)) === JSON.stringify({}));
-dm = {"a": [{}]};
-assert(JSON.stringify(initializeDAlgorithm(dm)) == JSON.stringify({ a: 0}));
-dm = {"a": [{ "node": "b", "weight": 10}],
-      "b": [{ "node": "c", "weight": 10}],
-      "c": [{ "node": "d", "weight": 10}],
-      "d": [{ "node": "e", "weight": 10}],
-      "e": [{ "node": "a", "weight": 10}]};
-assert(JSON.stringify(initializeDAlgorithm(dm)) == JSON.stringify({ a: 0, b: 10, c: 20, d: 30, e: 40 }));
-dm = {"a": [{ "node": "b", "weight": 10}],
-      "b": [{ "node": "c", "weight": 10}],
-      "c": [{ "node": "a", "weight": 10}],
-      "d": [{ "node": "e", "weight": 10}],
-      "e": [{ "node": "a", "weight": 10}]};
-assert(JSON.stringify(initializeDAlgorithm(dm)) == JSON.stringify({ a: 0, b: 10, c: 20, d: Infinity, e: Infinity }));
-dm = {"a": [{ "node": "b", "weight": 10}, { "node": "c", "weight": 1},{ "node": "e", "weight": 30}],
-      "b": [{ "node": "c", "weight": 10}],
-      "c": [{ "node": "d", "weight": 10}],
-      "d": [{ "node": "e", "weight": 10}],
-      "e": [{ "node": "a", "weight": 10}]};
-assert(JSON.stringify(initializeDAlgorithm(dm)) == JSON.stringify({ a: 0, b: 10, c: 1, d: 30, e: 30 }));
+
+var graph = {};
+assert(JSON.stringify(initializeDAlgorithm(graph)) == JSON.stringify({}));
+
+var graph1 = {'foo': {'boo': 10},
+    'boo': {'d': 10},
+    'd': {'e': 10},
+    'e': {'bar': 10},
+    'bar': {'foo': 2}};
+assert(JSON.stringify(initializeDAlgorithm(graph1)) == JSON.stringify({ foo: 0, boo: 10, d: 20, e: 30, bar: 40 }));
+
+var graph2 = {'foo': {}};
+assert(JSON.stringify(initializeDAlgorithm(graph2)) == JSON.stringify({ foo: 0 }));
+
+var graph3 = {'foo': {'boo': 10, 'd': 1, 'e': 2},
+    'boo': {'d': 10},
+    'd': {'e': 10},
+    'e': {'bar': 10},
+    'bar': {'foo': 2}};
+assert(JSON.stringify(initializeDAlgorithm(graph3)) == JSON.stringify({ foo: 0, boo: 10, d: 1, e: 2, bar: 12 }));
